789ea4a0ef14c5228af41bab15152a730db8e276
zachhsiao106 <zachhsiao106@gmail.com>
2025-07-16 20:00:52 +0800

    0.0.0

---
+++ src/main/java/net/zachhsiao106/myfirstmod/MyFirstMod.java
@@ -0,0 +1,16 @@
+package net.zachhsiao106.myfirstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyFirstMod implements ModInitializer {
+	public static final String MOD_ID = "my-first-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/zachhsiao106/myfirstmod/MyFirstModClient.java
@@ -0,0 +1,10 @@
+package net.zachhsiao106.myfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
